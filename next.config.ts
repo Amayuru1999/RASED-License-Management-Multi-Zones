3765ea57e4b1a7344c0548af549ebdb177057e3c
Amayuru1999 <amarasingheau@gmail.com>
2026-05-13 11:28:56 +0530

    feat: Implement shared UI

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from 'next'
-
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
+    externalDir: true,
+  },
   basePath: '/licenses',
   assetPrefix: '/licenses',
+  transpilePackages: ['rased-shared-ui'],
 }
 
 export default nextConfig
